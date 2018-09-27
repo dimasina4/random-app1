9045eabd02b827e3bb691d5bfaba5079345f0272
Dmitrii Stoilik <dstojlik@gmail.com>
2018-09-27 17:59:15 +0300

    Service

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropType from "prop-types";
+import Service from "./components/Service";
+import List from "./components/List";
+
+// Material
+import { withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { blue, red } from "@material-ui/core/colors";
+import Paper from "@material-ui/core/Paper";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import { Person, ListAlt } from "@material-ui/icons";
+
+// CSS
+import "./css/style.css";
+
+const styles = theme => ({
+  mainContent: {
+    height: "calc(100vh - 56px)",
+    overflow: "auto"
+  },
+  botNav: {
+    position: "fixed",
+    width: "100%",
+    bottom: 0
+  }
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: red
+  }
+});
 
 class App extends Component {
+  state = {
+    value: 1
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { value } = this.state;
+    const { classes } = this.props;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CssBaseline />
+        <MuiThemeProvider theme={theme}>
+          <div className={classes.mainContent}>
+            {this.state.value === 0 ? <List /> : <Service />}
+          </div>
+          <Paper elevation={2} className={classes.botNav}>
+            <BottomNavigation
+              value={value}
+              onChange={this.handleChange}
+              showLabels
+            >
+              <BottomNavigationAction label="List" icon={<Person />} />
+              <BottomNavigationAction label="Service" icon={<ListAlt />} />
+            </BottomNavigation>
+          </Paper>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 
-export default App;
+App.propType = {
+  classes: PropType.object.isRequired
+};
+
+export default withStyles(styles)(App);
---
+++ client/src/components/Service.js
@@ -0,0 +1,168 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { withStyles } from "@material-ui/core/styles";
+
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import { Refresh } from "@material-ui/icons";
+
+const styles = theme => ({
+  serviceTitle: {
+    flexGrow: 1
+  },
+  avatar: {
+    width: "50%",
+    height: "auto",
+    margin: "20px auto"
+  },
+  circularProgress: {
+    position: "absolute",
+    left: "calc(50% - 20px)",
+    marginTop: "40px"
+  },
+  tableWrapper: {
+    margin: "20px"
+  },
+  tcText: {
+    textTransform: "capitalize"
+  },
+  tcTextBold: {
+    fontWeight: "bold"
+  }
+});
+
+class Service extends Component {
+  state = {
+    user: {},
+    arrayToRender: []
+  };
+  getUserFromApi() {
+    axios
+      .get("https://randomuser.me/api")
+      .then(res => {
+        this.setState({ user: res.data.results[0] });
+        let arrayToRender = [
+          {
+            field1: "First Name",
+            field2: this.state.user.name.first
+          },
+          {
+            field1: "Last name",
+            field2: this.state.user.name.last
+          },
+          {
+            field1: "Age",
+            field2: this.state.user.dob.age
+          },
+          {
+            field1: "Gender",
+            field2: this.state.user.gender
+          },
+          {
+            field1: "E-mail",
+            field2: this.state.user.email
+          },
+          {
+            field1: "Phone",
+            field2: this.state.user.cell
+          },
+          {
+            field1: "Location",
+            field2:
+              this.state.user.location.state +
+              ", " +
+              this.state.user.location.city +
+              ", " +
+              this.state.user.location.street
+          }
+        ];
+        this.setState({ arrayToRender });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  handleRefreshButtonClick = () => {
+    this.setState({ user: {} });
+    this.getUserFromApi();
+  };
+  componentDidMount() {
+    this.getUserFromApi();
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <AppBar position="sticky">
+          <ToolBar>
+            <Typography
+              color="inherit"
+              variant="title"
+              className={classes.serviceTitle}
+            >
+              Service
+            </Typography>
+            <IconButton color="inherit" onClick={this.handleRefreshButtonClick}>
+              <Refresh />
+            </IconButton>
+          </ToolBar>
+        </AppBar>
+        {Object.keys(this.state.user).length === 0 &&
+        this.state.user.constructor === Object ? (
+          <div>
+            <CircularProgress
+              color="secondary"
+              className={classes.circularProgress}
+            />
+          </div>
+        ) : (
+          <Grid container spacing={0}>
+            <Grid item sm={4} xs={12}>
+              <Avatar
+                src={this.state.user.picture.large}
+                className={classes.avatar}
+              />
+            </Grid>
+
+            <Grid item sm={8} xs={12}>
+              <Paper className={classes.tableWrapper}>
+                <Table>
+                  <TableBody>
+                    {this.state.arrayToRender.map(item => (
+                      <TableRow key={item.field1}>
+                        <TableCell className={classes.tcTextBold}>
+                          {item.field1}:
+                        </TableCell>
+                        <TableCell padding="none" className={classes.tcText}>
+                          {item.field2}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </Grid>
+          </Grid>
+        )}
+      </div>
+    );
+  }
+}
+
+Service.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Service);
